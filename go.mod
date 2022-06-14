656244fdac05591b3f61639a52a5f396cb99a464
Constantin Rohde <rohdeconstantin@gmail.com>
2022-06-14 20:54:31 +0200

    Crawler can now create directory tree

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Haepaxlog/playngo
 
 go 1.18
 
-require github.com/veandco/go-sdl2 v0.4.21
+require (
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+	github.com/veandco/go-sdl2 v0.4.21
+)
--- lib/crawler.go
+++ lib/crawler.go
@@ -4,8 +4,77 @@ import (
 	"os/exec"
 	"bytes"
 	"regexp"
+	"io/ioutil"
+	"github.com/golang-collections/collections/stack"
+	//"path/filepath"
 )
 
+type Node struct{
+	path string
+	visited bool
+	children []*Node
+}
+
+func getNode(node *Node) (*Node, error){
+	// Read Subdirectories
+	entries, err := ioutil.ReadDir(node.path)
+	if err != nil {
+		return nil, err
+	}
+
+	// If No Children, Return Node with nil Children
+	if len(entries) == 0 {
+		if node.visited {
+			return &Node{node.path, node.visited, nil}, nil
+		}
+		return &Node{node.path, node.visited, nil}, nil
+	}
+
+	// If Node was already set, Return Node itself
+	if len(node.children) > 0{
+		return node, nil
+	}
+
+	// If Children, Return Node with Children
+	for _, entry := range entries {
+		if entry.IsDir(){
+			if node.visited {
+				node.children = append(node.children, &Node{node.path + "/" + entry.Name(), node.visited, nil})
+			}
+			node.children = append(node.children, &Node{node.path + "/" + entry.Name(), node.visited, nil})
+		}
+	}
+
+	return node, nil
+}
+
+
+//Call
+//	root := &Node{"/home/conni1410/Music/", false, nil}
+//	getTree(root)
+
+func getTree(node *Node) {
+	// Get Tree on Root Node with Stack
+	node, _ = getNode(node)
+	stack := stack.New()
+	stack.Push(node)
+	for stack.Len() > 0 {
+		node = stack.Peek().(*Node)
+		node, _ = getNode(node)
+		if node.visited || len(node.children) == 0 {
+			stack.Pop()
+			continue
+		}
+		for i := len(node.children) - 1; i>= 0; i-- {
+			stack.Push(node.children[i])
+		}
+		node.visited = true
+
+	}
+
+}
+
+// Deprecated but works
 func getDir(path string) ([]string, error){
 	bin := "ls"
 	arg1 := path
@@ -17,7 +86,7 @@ func getDir(path string) ([]string, error){
 		return nil, err
 	}
 
-	out :=  bytes.Split(stdout,[]byte("\n"))
+	out := bytes.Split(stdout,[]byte("\n"))
 	strOut := make([]string, len(out))
 
 	for i := 0; i < len(out); i++ {
